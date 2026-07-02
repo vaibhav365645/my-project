77553f8a5830c070e4411eb0f13d350c17004328
vaibhav365645 <javandhravaibhav2007@gmail.com>
2026-07-02 17:17:25 +0530

    style add

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo"hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="style" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            
+        </div>
+</header>
+    
+</body>
+</html>
