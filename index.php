bd30d8413c26e5e78d585c3b83e7875b5bad29c3
vaibhav365645 <javandhravaibhav2007@gmail.com>
2026-07-03 15:27:54 +0530

    change

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="style" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <div class="navbar">
-            
+                <p class="logo1 border"></p>
+                     
+                        <p>dashbord</p>
+                        <p>reports</p>
+                        <p>tool</p>
+                        <p>setting</p>
+                    
+
         </div>
 </header>
     
