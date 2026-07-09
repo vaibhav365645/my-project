8f7caf0bb7422ce7e00f878aa54ff3554468fdfa
vaibhav365645 <javandhravaibhav2007@gmail.com>
2026-07-09 22:02:52 +0530

    addprogresbar

--- Dashboard.php
+++ Dashboard.php
@@ -11,33 +11,96 @@
         <button type="button" >START SCAN</button>
     </div>
 
-    <!-- thi is progre sercal -->
-     <div class = progres>
+    <!-- thi is ovrall risk -->
+     <div class="wrapper">
+     <div class ="container">
+        <div class="outer">
+        <div class="inner">
+            <div id="nuber"> 65%</div>
+</div>
+</div>
 
-    <div class ="risk"id="risk1">
-        <?php echo"overall risk"; ?>
-        <div class="overall"></div>
-    </div>
+<svg width="200px" height="400px">
+    <defs>
+        <linearGradient id="linearGradient">
+            <stop offset="0%" stop-color="darkorange"/>
+            <stop offset="100%" stop-color="yellow"/>
+        </linearGradient>
+    </defs>
+    <circle cx="100" cy="200" r="85" />
+</svg>
+</div>
 
-    <div class ="risk" id="risk2">
-        <?php echo"overall risk"; ?>
-        <div class="overall"></div>
-    </div>
 
-    <div class ="risk"id="risk3">
-        <?php echo"overall risk"; ?>
-        <div class="overall"></div>
-    </div>
+ <!-- this is malware risk -->
+<div class ="container">
+        <div class="outer">
+        <div class="inner">
+            <div id="nuber"> 65%</div>
 
-    <div class ="risk"id="risk4">
-        <?php echo"overall risk"; ?>
-        <div class="overall"></div>
-    </div>
-    
+</div>
+</div>
+
+<svg width="200px" height="400px">
+    <defs>
+        <linearGradient id="linearGradient">
+            <stop offset="0%" stop-color="darkorange"/>
+            <stop offset="100%" stop-color="yellow"/>
+        </linearGradient>
+    </defs>
+    <circle cx="100" cy="200" r="85" />
+</svg>
+</div>
+
+<!-- thi is network risk-->
+     <div class="wrapper">
+     <div class ="container">
+        <div class="outer">
+        <div class="inner">
+            <div id="nuber"> 65%</div>
+</div>
+</div>
+
+<svg width="200px" height="400px">
+    <defs>
+        <linearGradient id="linearGradient">
+            <stop offset="0%" stop-color="darkorange"/>
+            <stop offset="100%" stop-color="yellow"/>
+        </linearGradient>
+    </defs>
+    <circle cx="100" cy="200" r="85" />
+</svg>
+</div>
+
+
+ <!-- this is web attacks-->
+<div class ="container">
+        <div class="outer">
+        <div class="inner">
+            <div id="nuber"> 65%</div>
+
+</div>
+</div>
+
+<svg width="200px" height="400px">
+    <defs>
+        <linearGradient id="linearGradient">
+            <stop offset="0%" stop-color="darkorange"/>
+            <stop offset="100%" stop-color="yellow"/>
+        </linearGradient>
+    </defs>
+    <circle cx="100" cy="200" r="85" />
+</svg>
 
 
 
 </div>
+
+
+
+
+
+
 
     
     
